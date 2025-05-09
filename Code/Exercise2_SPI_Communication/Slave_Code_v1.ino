465f13a22cae91207b9f97d25c34ad83c47673b5
Mohamed Aderak <91732940+MohamedAderak@users.noreply.github.com>
2025-05-10 00:09:33 +0100

    Update Slave_Code_v1.ino

--- Code/Exercise2_SPI_Communication/Slave_Code_v1.ino
+++ Code/Exercise2_SPI_Communication/Slave_Code_v1.ino
@@ -1,23 +1,22 @@
 #include <SPI.h>
-volatile char receivedData;
+
+char receivedData;
 
 void setup() {
   pinMode(MISO, OUTPUT);
   SPCR |= _BV(SPE);
-  SPI.attachInterrupt();
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
 }
 
-ISR(SPI_STC_vect) {
-  receivedData = SPDR;
-
-  if(receivedData == 'A') {
-    digitalWrite(8, !digitalRead(8));
-  }
-  else if(receivedData == 'B') {
-    digitalWrite(9, !digitalRead(9));
+void loop() {
+  if (SPSR & (1 << SPIF)) { 
+    receivedData = SPDR;
+    if (receivedData == 'A') {
+      digitalWrite(8, !digitalRead(8));
+    } 
+    else if (receivedData == 'B') {
+      digitalWrite(9, !digitalRead(9));
+    }
   }
 }
-
-void loop() {}
